49e601d3f8d90057ade835383fa99d290deac933
N.h.Dao <nhdao.it2.learn@gmail.com>
2025-06-29 18:18:16 +0700

    1. basic usage (typescript)

---
+++ src/stores/bearStore.tsx
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+
+type TBearStoreState = {
+  bears: number,
+  increasePopulation: () => void;
+  removeAllBears: () => void;
+  // getOwner:  () => Promise<string>;     // use "Promise" to wait for API
+}
+
+export const useBearStore = create<TBearStoreState>()((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({ 
+    bears: state.bears + 1 
+  })),
+  removeAllBears: () => set({ bears: 0 }),
+  // getOwner: async ()=> {
+  //   const response = await fetch("https://api/github/users/1");
+  //   const owner = await response.json();
+  //   console.log(owner.name);
+  //   return owner.name;
+  // }
+}));
