d8c621c9f7e869e77eb510d44aefd176b892ab4e
N.h.Dao <nhdao.it2.learn@gmail.com>
2025-06-30 23:22:49 +0700

    use setState to separate actions from the store__ much shorten code

--- src/stores/foodStore.ts
+++ src/stores/foodStore.ts
@@ -1,25 +1,20 @@
 import { create } from "zustand";
 import { persist, subscribeWithSelector, devtools } from "zustand/middleware";
 
-type TFoodStoreState = {
-    fish: number;
-    addOneFish: () => void;
-    removeOneFish: () => void;
-    removeAllFish: () => void;
+const initialFoodValue = {
+    fish: 0,
 }
 
-export const useFoodStore = create<TFoodStoreState>()(
-    devtools(subscribeWithSelector(persist((set) => ({
-    fish: 0,
-    addOneFish: () => set((state) => ({fish: state.fish + 1})),
-    removeOneFish: () => {
-        set((state) => ({ fish: state.fish - 1}));
-    },
-    removeAllFish: () => {
-        set({fish: 0});
-    }
-}),{
-    name: "food store persist",
-})), {
-    name: "food store devtools"
-}));
+export const useFoodStore = create<typeof initialFoodValue>()(
+    devtools(subscribeWithSelector(persist(() => (initialFoodValue),{name: "food store persist",})
+        ), 
+        {name: "food store devtools"}
+    )
+);
+
+export const addOneFish = () => 
+    useFoodStore.setState((state) => ({fish: state.fish + 1}));
+export const removeOneFish = () => 
+    useFoodStore.setState((state) => ({ fish: state.fish - 1}));
+export const removeAllFish = () => 
+    useFoodStore.setState({fish: 0});
